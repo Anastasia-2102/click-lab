3917ac7b87aeefa7440c9bba354d559439eb80e6
Anastasia <ketchersid18@mail.ru>
2026-08-19 13:40:44 -0500

    the page lives in real files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let answer1 = document.getElementById("answer-1");
+let answer2 = document.getElementById("answer-2");
+let answer3 = document.getElementById("answer-3");
+
+let result = document.getElementById("result-1");
+
+answer1.addEventListener("click", function () {
+  result.textContent = "Correct! 🎉";
+});
+
+answer2.addEventListener("click", function () {
+  result.textContent = "Not quite!";
+});
+
+answer3.addEventListener("click", function () {
+  result.textContent = "Not quite!";
+});
